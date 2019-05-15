4b45ba127e9e61fd256533df8d317fe1d13cb0f1
NANTHIRI <50017000+NANTHIRI@users.noreply.github.com>
2019-05-15 13:03:14 +0630

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 
 for (var x = 1; x <= 100; x++) {
 
-if(x % 3 == 0)
+if(x % 3 == 0 && x % 5 == 0)
 {
-    document.write("Fizz");
+    document.write("FizzBuzz");
 }
-else if( x % 5 ==0){
-    document.write("<p>" + "Buzz" + "</p>");
+else if( x % 3 == 0){
+    document.write("<p>" + "Fizz" + "</p>");
+}
+else if (x % 5 == 0){
+    document.write("<p>"+ "Buzz" + "</p>");
 }
 else{
     document.write("<p> " + x + "</p>");
